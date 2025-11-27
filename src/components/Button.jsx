5e16d1be6c9b079e21ee29336191b54b847b265c
motasimdev <themotasimdev@gmail.com>
2025-11-28 00:28:10 +0600

    header responsive running

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({Tagname="button", children, className, ...props}) => {
   return (
-    <Tagname {...props} className={`rounded-sm py-4 px-[30px] cursor-pointer ${className}`}>{children}</Tagname>
+    <Tagname {...props} className={`rounded-sm py-2 px-6 lg:py-4 lg:px-[30px] cursor-pointer ${className}`}>{children}</Tagname>
   )
 }
 
