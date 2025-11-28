05346cf2e73d409618d9bbe63f0b68e08d7a8907
motasimdev <themotasimdev@gmail.com>
2025-11-28 08:55:27 +0600

    gurntee running

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../layout/Hero'
+import Gurntee from '../layout/Gurntee'
 
 const Home = () => {
   return (
     <>
     <Hero/>
+    <Gurntee/>
     </>
   )
 }
