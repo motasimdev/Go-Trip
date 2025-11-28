d9f37a31031b21fc2d722b89c6010bd7100eab76
motasimdev <themotasimdev@gmail.com>
2025-11-28 21:50:33 +0600

    hover added

--- src/components/SpecialOffers.jsx
+++ src/components/SpecialOffers.jsx
@@ -17,24 +17,24 @@ const SpecialOffers = () => (
           </div>
 
           <div className="grid md:grid-cols-3 gap-4 md:gap-4 lg:gap-8">
-            <div className="bg-[url(/src/assets/specialOffer1.png)] bg-no-repeat bg-center bg-cover px-11.5 pt-15 pb-20 lg:pb-[126px] rounded-sm">
+            <div className="bg-[url(/src/assets/specialOffer1.png)] bg-no-repeat bg-center bg-cover px-8 md:px-8 lg:px-11.5 pt-15 pb-20 lg:pb-[126px] rounded-sm">
               <h4 className="pb-6 lg:pb-8 lg:pr-[50px] text-2xl lg:text-[30px] text-white font-semibold">
                 Things to do on your trip
               </h4>
-              <Button className={"bg-white"}>Experinces</Button>
+              <Button className={"bg-white hover:bg-[#3554D1] hover:text-white transition-colors duration-300"}>Experinces</Button>
             </div>
-            <div className="bg-[url(/src/assets/specialOffer2.png)] bg-no-repeat bg-center bg-cover px-11.5 pt-15 pb-20 lg:pb-[126px] rounded-sm">
+            <div className="bg-[url(/src/assets/specialOffer2.png)] bg-no-repeat bg-center bg-cover px-8 md:px-8 lg:px-11.5 pt-15 pb-20 lg:pb-[126px] rounded-sm">
             <h5 className="text-[15px] text-white font-medium">Enjoy Summer Deals</h5>
-              <h4 className="pb-6 lg:pb-8 text-2xl lg:text-[30px] text-white font-semibold">
+              <h4 className="pb-6 lg:pb-[50px] text-2xl lg:text-[30px] text-white font-semibold">
                 Up to 70% Discount!
               </h4>
-              <Button className={"bg-white"}>Experinces</Button>
+                 <Button className={"bg-white hover:bg-[#3554D1] hover:text-white transition-colors duration-300"}>Experinces</Button>
             </div>
-            <div className="bg-[url(/src/assets/specialOffer3.png)] bg-no-repeat bg-center bg-cover px-11.5 pt-15 pb-20 lg:pb-[126px] rounded-sm">
+            <div className="bg-[url(/src/assets/specialOffer3.png)] bg-no-repeat bg-center bg-cover px-8 md:px-5 lg:px-11.5 pt-15 pb-20 lg:pb-[126px] rounded-sm">
               <h4 className="pb-6 lg:pb-8 lg:pr-[50px] text-2xl lg:text-[30px] text-white font-semibold">
                 Let your curiosity do the booking
               </h4>
-              <Button className={"bg-white"}>Experinces</Button>
+              <Button className={"bg-white hover:bg-[#3554D1] hover:text-white transition-colors duration-300"}>Experinces</Button>
             </div>
           </div>
         </div>
---
+++ src/components/layout/Trending.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../Container'
+
+const Trending = () => {
+  return (
+    <>
+    <section>
+        <Container className=""></Container>
+    </section>
+    </>
+  )
+}
+
+export default Trending
