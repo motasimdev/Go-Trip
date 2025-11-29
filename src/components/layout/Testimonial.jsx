3f88c73ad613baa8a38b78ff37b7da6d78900c43
motasimdev <themotasimdev@gmail.com>
2025-11-30 01:05:02 +0600

    testimonial done

--- src/components/layout/Testimonial.jsx
+++ src/components/layout/Testimonial.jsx
@@ -9,38 +9,44 @@ import Flex from "../Flex";
 const Testimonial = () => {
   return (
     <>
-      <section className="pt-15 lg:pt-[120px] lg:pb-[100px] px-3 lg:px-0 bg-[url(/src/assets/testimonialBg.png)] bg-no-repeat bg-cover bg-center">
+      <section className="pt-10 lg:pt-[120px] pb-10 lg:pb-[100px] px-3 lg:px-0 bg-[url(/src/assets/testimonialBg.png)] bg-no-repeat bg-cover bg-center">
         <Container>
           <div className="text-center">
-            <h4>Testimonials</h4>
+            <h4 className="pb-3 lg:pb-5 text-2xl lg:text-3xl text-textClr1 font-semibold">
+              Testimonials
+            </h4>
             <ParaBase className={"text-textClr2!"}>
               Interdum et malesuada fames ac ante ipsum
             </ParaBase>
-            <div className="w-15 m-auto">
+            <div className="py-8 lg:py-12.5 w-10 lg:w-15 m-auto">
               <img src={koma} alt="" className="w-full" />
             </div>
-            <h5>
+            <h5 className="max-w-[796px] m-auto leading-8 lg:leading-[45px] text-lg lg:text-[22px] text-textClr1 font-semibold">
               "Our family was traveling via bullet train between cities in Japan
               with our luggage - the location for this hotel made that so easy.
               Agoda price was fantastic. "
             </h5>
-            <p>Ali Tufan</p>
-            <Para>Product Manager, Apple Inc</Para>
-            <Flex className={"justify-center"}>
-              <div className="cursor-pointer">
-                <HiUserCircle className="text-[75px]" />
+            <div className="pt-8 lg:pt-12.5 pb-6 lg:pb-9">
+              <p className="pb-2 lg:pb-4 text-sm lg:text-[15px] text-textClr1 font-medium">Ali Tufan</p>
+              <Para className={"text-textClr2!"}>
+                Product Manager, Apple Inc
+              </Para>
+            </div>
+            <Flex className={"justify-center lg:gap-3"}>
+              <div className="cursor-pointer rounded-full border border-transparent hover:border-black transition-all duration-200">
+                <HiUserCircle className="text-6xl lg:text-[75px]" />
               </div>
-              <div className="cursor-pointer">
-                <HiUserCircle className="text-[75px]" />
+              <div className="cursor-pointer rounded-full border border-transparent hover:border-black transition-all duration-200">
+                <HiUserCircle className="text-6xl lg:text-[75px]" />
               </div>
-              <div className="cursor-pointer">
-                <HiUserCircle className="text-[75px]" />
+              <div className="cursor-pointer rounded-full border border-transparent hover:border-black transition-all duration-200">
+                <HiUserCircle className="text-6xl lg:text-[75px]" />
               </div>
-              <div className="cursor-pointer">
-                <HiUserCircle className="text-[75px]" />
+              <div className="cursor-pointer rounded-full border border-transparent hover:border-black transition-all duration-200">
+                <HiUserCircle className="text-6xl lg:text-[75px]" />
               </div>
-              <div className="cursor-pointer">
-                <HiUserCircle className="text-[75px]" />
+              <div className="cursor-pointer rounded-full border border-transparent hover:border-black transition-all duration-200">
+                <HiUserCircle className="text-6xl lg:text-[75px]" />
               </div>
             </Flex>
           </div>
