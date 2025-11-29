f9f8f72198ac2d35ea236602d5b252ceaf003709
motasimdev <themotasimdev@gmail.com>
2025-11-30 02:27:20 +0600

    insp done

--- src/components/layout/Inspiration.jsx
+++ src/components/layout/Inspiration.jsx
@@ -17,12 +17,12 @@ const Inspiration = () => {
                 subTitle={"Interdum et malesuada fames"}
               />
             </div>
-            <Flex>
-              <div className="flex items-center  justify-between w-[48%]">
-                <div className="w-[30%]">
+            <div className="lg:flex items-center">
+              <div className="md:flex items-center justify-between lg:w-[48%] pb-10 lg:pb-0">
+                <div className="md:w-[33%] lg:w-[30%]">
                   <img src={insOne} alt="" className="w-full" />
                 </div>
-                <div className="w-[65%] pr-17">
+                <div className="text-center md:text-left pt-5 md:pt-0 md:w-[65%] md:pr-17">
                   <p className="text-[15px] text-textClr2">April 06, 2022</p>
                   <h4 className="text-[22px] text-textClr1 font-semibold">
                     10 European ski destinations you should visit this winter
@@ -35,14 +35,15 @@ const Inspiration = () => {
               </div>
 
               {/* ================================= */}
-              <div className="flex items-center  justify-between w-[48%]">
-                <div className="w-[30%]">
+
+              <div className="md:flex items-center justify-between lg:w-[48%]">
+                <div className="md:w-[33%] lg:w-[30%]">
                   <img src={insOne} alt="" className="w-full" />
                 </div>
-                <div className="w-[65%] pr-17">
+                <div className="text-center md:text-left pt-5 md:pt-0 md:w-[65%] md:pr-17">
                   <p className="text-[15px] text-textClr2">April 06, 2022</p>
                   <h4 className="text-[22px] text-textClr1 font-semibold">
-                    Booking travel during Corona: good advice in an uncertain time
+                    10 European ski destinations you should visit this winter
                   </h4>
                   <p className="text-[15px] text-textClr2">
                     Ut enim ad minim veniam, quis nostrud exerc ullamco laboris
@@ -50,7 +51,7 @@ const Inspiration = () => {
                   </p>
                 </div>
               </div>
-            </Flex>
+            </div>
           </div>
         </Container>
       </section>
