8facbc7584f3e87da9097fab0afb6cbf99e7e931
motasimdev <themotasimdev@gmail.com>
2025-11-30 22:48:23 +0600

    adding animations

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -3,6 +3,7 @@ import ParaBase from "../ParaBase";
 import Container from "../Container";
 import { IoMdSearch } from "react-icons/io";
 import Flex from "../Flex";
+import 'animate.css';
 
 const Hero = () => {
   return (
@@ -10,13 +11,13 @@ const Hero = () => {
       <section className="bg-[url(/src/assets/bannerBg.png)] bg-no-repeat bg-cover bg-center px-3 lg:px-0 pt-10 md:pt-20 lg:pt-[375px] pb-10 md:pb-16 lg:pb-[295px]">
         <Container>
           <div className="text-center">
-            <h1 className="font-semibold text-3xl md:text-4xl lg:text-6xl text-white">
+            <h1 className="font-semibold text-3xl md:text-4xl lg:text-6xl text-white animate__animated  animate__fadeInUp animate__fast">
               The World is Waiting For You
             </h1>
-            <ParaBase className={"font-normal pt-3 lg:pt-5 pb-3 lg:pb-7"}>
+            <ParaBase className={"font-normal pt-3 lg:pt-5 pb-3 lg:pb-7 animate__animated  animate__fadeInUp"}>
               Discover amzaing places at exclusive deals
             </ParaBase>
-            <div className="bg-white rounded-2xl lg:rounded-full py-2.5 pl-8 md:max-w-[500px] lg:max-w-[900px] m-auto">
+            <div className="bg-white rounded-2xl lg:rounded-full py-2.5 pl-8 md:max-w-[500px] lg:max-w-[900px] m-auto animate__animated  animate__fadeInUp ">
               <div className="grid grid-cols-2 lg:grid-cols-4 gap-x-3 gap-y-3 lg:gap-y-0 items-center">
                 <div className="text-left">
                   <p
