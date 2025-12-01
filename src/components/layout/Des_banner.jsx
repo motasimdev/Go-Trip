8f856b3dde36f2567dbe2ffa95cb91c1fcc26165
motasimdev <themotasimdev@gmail.com>
2025-12-01 23:39:37 +0600

    banner done

--- src/components/layout/Des_banner.jsx
+++ src/components/layout/Des_banner.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import Container from "../Container";
 import ParaBase from "../ParaBase";
+import Button from "../Button";
 
 const Des_banner = () => {
   return (
     <>
       <section className="">
         <Container>
-          <div className="bg-[url(/src/assets/Des-banner.png)] bg-no-repeat bg-cover bg-center px-3 lg:pr-0 lg:pl-11 pt-10 md:pt-16 lg:pt-[70px] pb-30 md:pb-35 lg:pb-[355px] ">
+          <div className="relative bg-[url(/src/assets/Des-banner.png)] bg-no-repeat bg-cover bg-center px-3 lg:pr-0 lg:pl-11 pt-10 md:pt-16 lg:pt-[70px] pb-30 md:pb-35 lg:pb-[355px] ">
             <h1 className="text-3xl lg:text-[50px] text-white font-semibold">
               Explore London
             </h1>
             <ParaBase>
               Explore deals, travel guides and things to do in London
             </ParaBase>
+            <Button
+              className={
+                "text-textClr1 bg-white hover:text-white hover:bg-textClr3 transition-colors duration-300 absolute right-3 bottom-3"
+              }
+            >
+              See All 90 Photos
+            </Button>
           </div>
         </Container>
       </section>
