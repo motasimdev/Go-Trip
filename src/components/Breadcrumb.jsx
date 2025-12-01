88162c15e93cf1c0fd48160203de420de545ca7a
motasimdev <themotasimdev@gmail.com>
2025-12-01 22:29:47 +0600

    destination page running

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Para from './Para'
-import { FaChevronRight } from "react-icons/fa";
+import React from "react";
+import Para from "./Para";
+import { RxCaretRight } from "react-icons/rx";
+import Container from "./Container";
+import Flex from "./Flex";
+import { Link } from "react-router";
 
 const Breadcrumb = () => {
   return (
-    <div>
-        <Para className={"text-textClr2"}>Europe  <FaChevronRight/>  United Kingdom (UK)  <FaChevronRight/>  <span className='text-textClr1'>London</span></Para>
-    </div>
-  )
-}
+    <>
+      <section className="pt-[115px]">
+        <Container>
+          <Flex className={"justify-between"}>
+            <Para className={"text-textClr2!"}>
+              <Flex>
+                <Link className="cursor-pointer">Europe</Link>{" "}
+                <RxCaretRight className="text-lg" />{" "}
+                <Link className="cursor-pointer">United Kingdom (UK)</Link>{" "}
+                <RxCaretRight className="text-lg" />{" "}
+                <Link className="text-textClr1 cursor-pointer">London</Link>
+              </Flex>
+            </Para>
+            <Para className={"text-textClr2!"}>
+              London Tourism: Best of London
+            </Para>
+          </Flex>
+        </Container>
+      </section>
+    </>
+  );
+};
 
-export default Breadcrumb
+export default Breadcrumb;
