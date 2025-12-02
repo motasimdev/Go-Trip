e5f9d34ecf0d3a0efa21c2eb65cfdd3dd1afadb4
motasimdev <themotasimdev@gmail.com>
2025-12-02 20:36:45 +0600

    Des_topsifhts runnning

---
+++ src/components/layout/Destination/Des_topSights.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Container from "../../Container";
+
+const Des_topSights = () => {
+  return (
+    <>
+      <section className="">
+        <Container>
+          <div className="">
+            <div
+              className="mb-8 md:mb-9 lg:mb-11 text-center md:text-left"
+              data-aos="fade-up"
+            >
+              <Heading
+                title={"Travel guides & tips for London"}
+                subTitle={"Interdum et malesuada fames ac ante ipsum"}
+              />
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Des_topSights;
