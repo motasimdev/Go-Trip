51e6b497059bd5e831811f5eb87ce2d8e6f3d905
motasimdev <themotasimdev@gmail.com>
2025-12-03 22:22:52 +0600

    con_choose running

---
+++ src/components/layout/contact/Con_choose.jsx
@@ -0,0 +1,18 @@
+import Container from "@/components/Container";
+import React from "react";
+
+const Con_choose = () => {
+  return (
+    <>
+      <section className="pt-1 md:pt-16 lg:pt-20 px-3 lg:px-0">
+        <Container>
+            <div className="">
+                
+            </div>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Con_choose;
