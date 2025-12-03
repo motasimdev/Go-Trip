4caa3fb3a89e5b6a131f792c6f3772a08d17f4df
motasimdev <themotasimdev@gmail.com>
2025-12-04 00:01:40 +0600

    blog_banner done

--- src/components/layout/blog/Blog_banner.jsx
+++ src/components/layout/blog/Blog_banner.jsx
@@ -6,7 +6,7 @@ import banner from "@/assets/blog/banner.png"
 const Blog_banner = () => {
   return (
     <>
-      <section className="py-7.5 lg:py-10 px-3 lg:px-0">
+      <section className="py-4 md:py-7.5 lg:py-10 px-3 lg:px-0">
         <Container>
           <div
             className="mb-8 md:mb-9 lg:mb-11 text-center"
@@ -17,7 +17,7 @@ const Blog_banner = () => {
               title={"Top 13 must-see natural wonders in Indonesia"}
               subTitle={"April 06, 2022"}
             />
-            <div className="mt-10">
+            <div className="mt-5 md:mt-10">
                 <img src={banner} alt="" />
             </div>
           </div>
