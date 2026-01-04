ff244714610b1990a3b4e5910ccea394181781e4
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-04 23:47:00 +0600

    dsf

--- src/components/layout/blog/Share.jsx
+++ src/components/layout/blog/Share.jsx
@@ -1,7 +1,7 @@
 import Flex from "@/components/Flex";
 import ParaBase from "@/components/ParaBase";
 import React from "react";
-import { FaFacebookF } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { IoLogoTwitter } from "react-icons/io";
 
 const Share = () => {
@@ -9,7 +9,31 @@ const Share = () => {
     <>
       <section>
         <div className="max-w-[850px] m-auto">
-          
+          <Flex>
+            <ParaBase text={"Share"} className={"font-medium"} />
+            <Flex className={"gap-x-1 "}>
+              <Link>
+                <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#ffffff5a] transition-colors duration-300">
+                  <FaFacebookF className="text-white" />
+                </div>
+              </Link>
+              <Link>
+                <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#ffffff5a] transition-colors duration-300">
+                  <IoLogoTwitter className="text-white" />
+                </div>
+              </Link>
+              <Link>
+                <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#ffffff5a] transition-colors duration-300">
+                  <FaInstagram className="text-white" />
+                </div>
+              </Link>
+              <Link>
+                <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#ffffff5a] transition-colors duration-300">
+                  <FaLinkedinIn className="text-white" />
+                </div>
+              </Link>
+            </Flex>
+          </Flex>
         </div>
       </section>
     </>
