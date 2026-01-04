7d6b812f39eef60bded7631df6423cba3895fe7d
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-04 23:57:47 +0600

    afda

--- src/components/layout/blog/Blog_share.jsx
+++ src/components/layout/blog/Blog_share.jsx
@@ -8,32 +8,35 @@ import { Link } from "react-router";
 const Blog_share = () => {
   return (
     <>
-      <section>
+      <section className="py-2 lg:pt-6 px-3 lg:px-0">
         <div className="max-w-[850px] m-auto">
-          <Flex>
-            <ParaBase text={"Share"} className={"text-black!"} />
-            <Flex className={"gap-x-1 "}>
-              <Link>
-                <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
-                  <FaFacebookF className="text-textClr1" />
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
-                  <IoLogoTwitter className="text-textClr1" />
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
-                  <FaInstagram className="text-textClr1" />
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
-                  <FaLinkedinIn className="text-textClr1" />
-                </div>
-              </Link>
+          <Flex className={"justify-between"}>
+            <Flex className="">
+              <ParaBase text={"Share"} className={"text-black! pr-5"} />
+              <Flex className={"gap-x-1 "}>
+                <Link>
+                  <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
+                    <FaFacebookF className="text-textClr1" />
+                  </div>
+                </Link>
+                <Link>
+                  <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
+                    <IoLogoTwitter className="text-textClr1" />
+                  </div>
+                </Link>
+                <Link>
+                  <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
+                    <FaInstagram className="text-textClr1" />
+                  </div>
+                </Link>
+                <Link>
+                  <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
+                    <FaLinkedinIn className="text-textClr1" />
+                  </div>
+                </Link>
+              </Flex>
             </Flex>
+            <div className=""></div>
           </Flex>
         </div>
       </section>
