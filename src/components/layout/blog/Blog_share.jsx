0874b9866bb0de99597dd47d8134fd7907f3703e
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 00:07:50 +0600

    asfsadf

--- src/components/layout/blog/Blog_share.jsx
+++ src/components/layout/blog/Blog_share.jsx
@@ -1,4 +1,5 @@
 import Flex from "@/components/Flex";
+import Para15 from "@/components/Para15";
 import ParaBase from "@/components/ParaBase";
 import React from "react";
 import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
@@ -11,8 +12,8 @@ const Blog_share = () => {
       <section className="py-2 lg:pt-6 px-3 lg:px-0">
         <div className="max-w-[850px] m-auto">
           <Flex className={"justify-between"}>
-            <Flex className="">
-              <ParaBase text={"Share"} className={"text-black! pr-5"} />
+            <Flex className={"gap-4"}>
+              <ParaBase text={"Share"} className={"text-black!"} />
               <Flex className={"gap-x-1 "}>
                 <Link>
                   <div className="py-2 px-2 lg:py-4 lg:px-4 rounded-full hover:bg-[#F5F5F5] transition-colors duration-300">
@@ -36,7 +37,17 @@ const Blog_share = () => {
                 </Link>
               </Flex>
             </Flex>
-            <div className=""></div>
+            {/* =============== */}
+            <Flex className="">
+              <button className="group py-1.5 px-5 bg-[#3554d10c] rounded-full cursor-pointer hover:bg-textClr3 transition-colors duration-200">
+                <Para15
+                  text={"Museum"}
+                  className={
+                    "text-textClr3 font-medium group-hover:text-white transition-colors duration-200"
+                  }
+                />
+              </button>
+            </Flex>
           </Flex>
         </div>
       </section>
