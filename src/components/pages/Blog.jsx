efca611dcf96775740c7faa4fbf6f94b6ea6c117
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 22:43:48 +0600

    blog review part start

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -4,6 +4,7 @@ import Blog_banner from "../layout/blog/Blog_banner";
 import Blog_reviewOne from "../layout/blog/Blog_reviewOne";
 import Blog_share from "../layout/blog/Blog_share";
 import Blog_testimonial from "../layout/blog/Blog_testimonial";
+import Blog_review from "../layout/blog/Blog_review";
 
 const Blog = () => {
   return (
@@ -17,6 +18,7 @@ const Blog = () => {
       <Blog_reviewOne/>
       <Blog_share/>
       <Blog_testimonial/>
+      <Blog_review/>
     </>
   );
 };
