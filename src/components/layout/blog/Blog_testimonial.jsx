9921ef2294447bb6e638b68c0a79d2d82af00b63
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 19:27:36 +0600

    blog testimonial running

--- src/components/layout/blog/Blog_testimonial.jsx
+++ src/components/layout/blog/Blog_testimonial.jsx
@@ -10,15 +10,18 @@ const Blog_testimonial = () => {
     <>
     <section className='py-2 lg:pt-6 px-3 lg:px-0'>
         <div className="max-w-[850px] m-auto">
-            <div className={"flex gap-x-5"}>
-                <div className="lg:h-[70px] lg:w-[70px] overflow-hidden">
+            <div className={"flex gap-x-5 border-t border-b py-7"}>
+                <div className="h-[70px] w-[70px] overflow-hidden">
                     <img src={user} alt="user" className='w-full'/>
                 </div>
                 <div className="">
                     <ParaBase text={"Brooklyn Simmons"} className={"text-textClr1! text-lg!"}/>
                     <Para text={"Medical Assistant"} className={"text-textClr2!"}/>
-                    <Para15 text={"Etiam vitae leo et diam pellentesque porta. Sed eleifend ultricies risus, vel rutrum erat commodo ut. Praesent finibus congue euismod. Nullam scelerisque massa vel augue placerat, a tempor sem egestas. Curabitur placerat finibus lacus."} className={"text-textClr1! max-w-[670px]"}/>
+                    <Para15 text={"Etiam vitae leo et diam pellentesque porta. Sed eleifend ultricies risus, vel rutrum erat commodo ut. Praesent finibus congue euismod. Nullam scelerisque massa vel augue placerat, a tempor sem egestas. Curabitur placerat finibus lacus."} className={"text-textClr1! max-w-[670px] line-clamp-3 lg:line-clamp-none text-justify"}/>
                 </div>
+            </div>
+            <div className="flex justify-between">
+                
             </div>
         </div>
     </section>
