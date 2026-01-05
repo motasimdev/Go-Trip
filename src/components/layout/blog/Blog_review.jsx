6d90a24e54ff6bcde78692a4fdc8372a450f0190
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 23:02:07 +0600

    blog review part start

--- src/components/layout/blog/Blog_review.jsx
+++ src/components/layout/blog/Blog_review.jsx
@@ -3,24 +3,28 @@ import ParaBase from "@/components/ParaBase";
 import React from "react";
 import user from "/src/assets/blog/user.png";
 import Para from "@/components/Para";
+import Para15 from "@/components/Para15";
 
 const Blog_review = () => {
   return (
     <>
       <section className="py-2 lg:pt-6 px-3 lg:px-0">
         <div className="max-w-[850px] m-auto">
-          <h6 className="text-lg font-medium md:text-[22px] text-textClr1">
+          <h6 className="pb-4 text-lg font-medium md:text-[22px] text-textClr1">
             Guest reviews
           </h6>
-          <div className="flex gap-x-3">
+          <Flex className="gap-x-3">
             <div className="w-10 lg:h-[70px] h-10 lg:w-[70px] overflow-hidden">
               <img src={user} alt="user" className="w-full" />
             </div>
             <div className="">
               <ParaBase text={"Tonko"} className={"text-textClr1!"} />
-              <Para text={"March 2022"} className={"text-textClr2"} />
+              <Para text={"March 2022"} className={"text-textClr2!"} />
             </div>
-          </div>
+          </Flex>
+
+          <ParaBase text={"9.2 Superb"} className={"text-textClr3! py-5"}/>
+          <Para15 text={"Nice two level apartment in great London location. Located in quiet small street, but just 50 meters from main street and bus stop. Tube station is short walk, just like two grocery stores. "} className={"text-textClr1"}/>
         </div>
       </section>
     </>
