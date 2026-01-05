21d57a025d526526e6b1248641e256b3e01238a1
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 23:45:12 +0600

    blog review part start

--- src/components/layout/blog/Blog_review.jsx
+++ src/components/layout/blog/Blog_review.jsx
@@ -16,60 +16,62 @@ const Blog_review = () => {
     <>
       <section className="py-2 lg:pt-6 px-3 lg:px-0">
         <div className="max-w-[850px] m-auto">
-          <h6 className="pb-4 text-lg font-medium md:text-[22px] text-textClr1">
-            Guest reviews
-          </h6>
-          <Flex className="gap-x-3">
-            <div className="w-10 lg:h-[70px] h-10 lg:w-[70px] overflow-hidden">
-              <img src={user} alt="user" className="w-full" />
-            </div>
-            <div className="">
-              <ParaBase text={"Tonko"} className={"text-textClr1!"} />
-              <Para text={"March 2022"} className={"text-textClr2!"} />
-            </div>
-          </Flex>
+          <div className="">
+            <h6 className="pb-4 text-lg font-medium md:text-[22px] text-textClr1">
+              Guest reviews
+            </h6>
+            <Flex className="gap-x-3">
+              <div className="w-10 lg:h-[70px] h-10 lg:w-[70px] overflow-hidden">
+                <img src={user} alt="user" className="w-full" />
+              </div>
+              <div className="">
+                <ParaBase text={"Tonko"} className={"text-textClr1!"} />
+                <Para text={"March 2022"} className={"text-textClr2!"} />
+              </div>
+            </Flex>
 
-          <ParaBase
-            text={"9.2 Superb"}
-            className={"text-textClr3! py-3 lg:py-5"}
-          />
-          <Para15
-            text={
-              "Nice two level apartment in great London location. Located in quiet small street, but just 50 meters from main street and bus stop. Tube station is short walk, just like two grocery stores. "
-            }
-            className={"text-textClr1"}
-          />
-          <Flex className={"gap-x-3 lg:gap-x-5"}>
-            <div className="w-20 h-20 lg:w-[120px] lg:h-[120px]">
-              <img src={reviwPicOne} alt="" className="w-full rounded-md" />
-            </div>
-            <div className="w-20 h-20 lg:w-[120px] lg:h-[120px]">
-              <img src={reviwPicTwo} alt="" className="w-full rounded-md" />
-            </div>
-            <div className="w-20 h-20 lg:w-[120px] lg:h-[120px]">
-              <img src={reviwPicThree} alt="" className="w-full rounded-md" />
-            </div>
-            <div className="w-20 h-20 lg:w-[120px] lg:h-[120px]">
-              <img src={reviwPicFour} alt="" className="w-full rounded-md" />
-            </div>
-          </Flex>
-          
-          {/* ========================= */}
-          
-          <Flex>
-            <Link>
-              <Flex>
-                <AiFillLike className="text-textClr2" />
-                <Para15 text={"Helpful "} className={"text-textClr2!"} />
-              </Flex>
-            </Link>
-            <Link>
-              <Flex>
-                <AiFillDislike className="text-textClr2" />
-                <Para15 text={"Not helpful "} className={"text-textClr2!"} />
-              </Flex>
-            </Link>
-          </Flex>
+            <ParaBase
+              text={"9.2 Superb"}
+              className={"text-textClr3! py-3 lg:py-5"}
+            />
+            <Para15
+              text={
+                "Nice two level apartment in great London location. Located in quiet small street, but just 50 meters from main street and bus stop. Tube station is short walk, just like two grocery stores. "
+              }
+              className={"text-textClr1"}
+            />
+            <Flex className={"gap-x-3 lg:gap-x-5 py-3 lg:py-6"}>
+              <div className="w-20 h-20 lg:w-[120px] lg:h-[120px]">
+                <img src={reviwPicOne} alt="" className="w-full rounded-md" />
+              </div>
+              <div className="w-20 h-20 lg:w-[120px] lg:h-[120px]">
+                <img src={reviwPicTwo} alt="" className="w-full rounded-md" />
+              </div>
+              <div className="w-20 h-20 lg:w-[120px] lg:h-[120px]">
+                <img src={reviwPicThree} alt="" className="w-full rounded-md" />
+              </div>
+              <div className="w-20 h-20 lg:w-[120px] lg:h-[120px]">
+                <img src={reviwPicFour} alt="" className="w-full rounded-md" />
+              </div>
+            </Flex>
+
+            {/* ========================= */}
+
+            <Flex className={"gap-x-7"}>
+              <Link>
+                <Flex>
+                  <AiFillLike className="text-textClr2" />
+                  <Para15 text={"Helpful "} className={"text-textClr2!"} />
+                </Flex>
+              </Link>
+              <Link>
+                <Flex>
+                  <AiFillDislike className="text-textClr2" />
+                  <Para15 text={"Not helpful "} className={"text-textClr2!"} />
+                </Flex>
+              </Link>
+            </Flex>
+          </div>
         </div>
       </section>
     </>
