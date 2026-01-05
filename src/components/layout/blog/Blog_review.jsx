7786a8c5ff5055ea511e4a92d48dc57efcb00588
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 22:43:22 +0600

    blog review part start

--- src/components/layout/blog/Blog_review.jsx
+++ src/components/layout/blog/Blog_review.jsx
@@ -2,6 +2,7 @@ import Flex from '@/components/Flex'
 import ParaBase from '@/components/ParaBase'
 import React from 'react'
 import user from '/src/assets/blog/user.png'
+import Para from '@/components/Para'
 
 const Blog_review = () => {
   return (
@@ -10,7 +11,11 @@ const Blog_review = () => {
         <div className="max-w-[850px] m-auto">
             <h6 className='font-medium text-[22px] text-textClr1'>Guest reviews</h6>
             <Flex>
-                <img src={user} alt="" />
+                <img src={user} alt="user" />
+                <div className="">
+                    <ParaBase text={"Tonko"} className={"text-textClr1!"}/>
+                    <Para text={"March 2022"} className={"text-textClr2"}/>
+                </div>
             </Flex>
         </div>
     </section>
