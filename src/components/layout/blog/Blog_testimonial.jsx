1365041d65705b552664d85ad222d7f0c9312e37
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 19:34:37 +0600

    blog testimonial running

--- src/components/layout/blog/Blog_testimonial.jsx
+++ src/components/layout/blog/Blog_testimonial.jsx
@@ -4,13 +4,14 @@ import user from '/src/assets/blog/user.png'
 import ParaBase from '@/components/ParaBase'
 import Para from '@/components/Para'
 import Para15 from '@/components/Para15'
-
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { FaArrowRightLong } from "react-icons/fa6";
 const Blog_testimonial = () => {
   return (
     <>
     <section className='py-2 lg:pt-6 px-3 lg:px-0'>
         <div className="max-w-[850px] m-auto">
-            <div className={"flex gap-x-5 border-t border-b py-7"}>
+            <div className={"flex gap-x-5 border-t border-b py-4 lg:py-7"}>
                 <div className="h-[70px] w-[70px] overflow-hidden">
                     <img src={user} alt="user" className='w-full'/>
                 </div>
@@ -20,8 +21,14 @@ const Blog_testimonial = () => {
                     <Para15 text={"Etiam vitae leo et diam pellentesque porta. Sed eleifend ultricies risus, vel rutrum erat commodo ut. Praesent finibus congue euismod. Nullam scelerisque massa vel augue placerat, a tempor sem egestas. Curabitur placerat finibus lacus."} className={"text-textClr1! max-w-[670px] line-clamp-3 lg:line-clamp-none text-justify"}/>
                 </div>
             </div>
-            <div className="flex justify-between">
-                
+            <div className="flex justify-between border-b py-4 lg:py-7">
+                <div className="">
+                    <FaArrowLeftLong/>
+                </div>
+                <div className="cols-1">
+                    <FaArrowRightLong/>
+                </div>
+
             </div>
         </div>
     </section>
