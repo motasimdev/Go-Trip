62a761ddf89d10a8035972e164c70610386b6b5a
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 22:41:29 +0600

    blog review part start

--- src/components/layout/blog/Blog_review.jsx
+++ src/components/layout/blog/Blog_review.jsx
@@ -1,9 +1,19 @@
+import Flex from '@/components/Flex'
+import ParaBase from '@/components/ParaBase'
 import React from 'react'
+import user from '/src/assets/blog/user.png'
 
 const Blog_review = () => {
   return (
     <>
-    <section className='py-2 lg:pt-6 px-3 lg:px-0'></section>
+    <section className='py-2 lg:pt-6 px-3 lg:px-0'>
+        <div className="max-w-[850px] m-auto">
+            <h6 className='font-medium text-[22px] text-textClr1'>Guest reviews</h6>
+            <Flex>
+                <img src={user} alt="" />
+            </Flex>
+        </div>
+    </section>
     </>
   )
 }
