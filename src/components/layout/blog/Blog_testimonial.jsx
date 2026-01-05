486a84cf2e031433fb8a4050cb1bce835642e4f7
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-05 19:07:18 +0600

    blog testimonial running

--- src/components/layout/blog/Blog_testimonial.jsx
+++ src/components/layout/blog/Blog_testimonial.jsx
@@ -1,10 +1,18 @@
+import Flex from '@/components/Flex'
 import React from 'react'
+
 
 const Blog_testimonial = () => {
   return (
     <>
     <section className='py-2 lg:pt-6 px-3 lg:px-0'>
-    
+        <div className="max-w-[850px] m-auto">
+            <Flex>
+                <div className="lg:h-[70px] lg:w-[70px]">
+                    <img src="" alt="" className=''/>
+                </div>
+            </Flex>
+        </div>
     </section>
     </>
   )
