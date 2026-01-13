7d82c6308ff42368d07f9d0657a76e7bcd8f38c5
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-13 23:33:07 +0600

    About page banner

--- src/components/layout/about/About_banner.jsx
+++ src/components/layout/about/About_banner.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const About_banner = () => {
   return (
     <>
-    <script className='py-20 lg:py-20 px-3 lg:px-0'>
+    <script className='py-20 lg:py-20 px-3 lg:px-0 bg-[url(/src/assets)]'>
         <Container>
             
         </Container>
