d87c5125ce61c59f074d2ba77355b422f495dece
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-15 00:23:15 +0600

    about why choose running

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About_banner from '../layout/about/About_banner'
+import About_whyChoose from '../layout/about/About_whyChoose'
 
 const About = () => {
   return (
     <>
     <About_banner/>
+    <About_whyChoose/>
     </>
   )
 }
