e4a0db16136f4549ab4f2630bdd48fd37f59d7de
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-15 00:10:31 +0600

    about banner done

--- src/components/layout/about/About_banner.jsx
+++ src/components/layout/about/About_banner.jsx
@@ -1,19 +1,22 @@
-import Container from '@/components/Container'
-import React from 'react'
+import Container from "@/components/Container";
+import ParaBase from "@/components/ParaBase";
+import React from "react";
 
 const About_banner = () => {
   return (
     <>
-    <script className='py-20 lg:py-20 px-3 lg:px-0 bg-[url(/src/assets/about/1.png)]'>
+      <section className="py-16 lg:py-20 px-3 lg:px-0 bg-[url(/src/assets/about/1.png)]">
         <Container>
-          <p className="bg-red-800">
-            asfasfa
-
-          </p>
+          <div className="text-center">
+            <h3 className="font-semibold text-3xl lg:text-[40px] text-white pb-2 lg:pb-3">
+              Looking for joy?
+            </h3>
+            <ParaBase text={"Your trusted trip companion"}/>
+          </div>
         </Container>
-    </script>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default About_banner
+export default About_banner;
